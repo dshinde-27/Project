7d0503d82a25f7545e97693cabedacd5af5dba58
dshinde-27 <dhanashreeshinde1027@gmail.com>
2025-06-02 14:42:57 +0530

    Initial commit

--- AdminTool_Backend/CRM_Api/CRM_Api/obj/Debug/net8.0/CRM_Api.AssemblyInfo.cs
+++ AdminTool_Backend/CRM_Api/CRM_Api/obj/Debug/net8.0/CRM_Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+853cefea218afdd63dab986bb617f70c83bd6868")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+154822642eb5422f6b115e21b8262554f03417e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AdminTool_Frontend/admin_crm/src/App.js
+++ AdminTool_Frontend/admin_crm/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Authentication/Login';
+import Register from './Authentication/Register';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const AuthenticatedLayout = ({ children }) => (
+    <div className="app-layout">
+      {/* <Sidebar /> */}
+      <div className="main-content">
+        {children}
+      </div>
     </div>
+  );
+  return (
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Protected Routes with Sidebar */}
+        {/* <Route path="/role" element={
+          <AuthenticatedLayout><Role /></AuthenticatedLayout>
+        } />  */}
+      </Routes>
+    </Router>
   );
 }
 
